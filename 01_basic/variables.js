475fb6c7d2eae7a239bdf7eada0e9873f1251cb0
Raushan Kumar <92165794+Raushan75@users.noreply.github.com>
2024-02-23 08:12:22 +0000

    Container variable

---
+++ 01_basic/variables.js
@@ -0,0 +1,17 @@
+const accountId =3244
+let accountEmail ="Rk2356@gmail.com"
+var accountPassword="12345"
+accountCity="Patna"
+let accountState;  //undefined
+
+// accountId=23 //not allow for change
+accountEmail="hafe@gmail.com"
+accountPassword="34323"
+accountCity="rachi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in  block scope and functional scope
+*/ 
