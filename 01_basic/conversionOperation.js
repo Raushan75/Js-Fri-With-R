82ecaa9c9549196b437c3ad828c14967d80bed8f
Raushan Kumar <92165794+Raushan75@users.noreply.github.com>
2024-02-23 13:39:10 +0000

    conversion of datatype

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,26 @@
+let score="33"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+let valueInNumber=Number (score)
+// console.log(typeof valueInNumber);
+
+
+//"33"=> 33
+//"33ac"=>NaN
+//true=>1;false=>0
+let isloggedIn=1
+
+let booleanIsloggedIn=Boolean(isloggedIn)
+// console.log(booleanIsloggedIn)
+
+//1=>true
+//0=>false
+//"raushan"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
