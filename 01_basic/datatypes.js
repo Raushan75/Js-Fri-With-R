ae8ba73a6c7438cfb24c466c58a9bbf2beebae92
Raushan Kumar <92165794+Raushan75@users.noreply.github.com>
2024-02-23 08:42:23 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alter(3+5) // we are using nodesjs, not browser 
+
+AudioListener("hello brother")
+
+console.log(4
+    +
+    5); // code readability should be high
+
+// number => 2 to power 53
+//bigint 
+//string => " "
+// booleand => true/false
+//null => standlone value
+// undefined => 
+//symbol => unique
+
+
+// object 
+
+console.log(typeof null);//objec
+console.log(typeof undefined)//undefined
